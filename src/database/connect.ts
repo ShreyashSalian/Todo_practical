f8b54c542a620159c00d036478efeb5af3beff05
ShreyashSalian <shreyashsalian15@gmail.com>
2025-03-25 08:53:28 +0530

    new changes

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -2,19 +2,12 @@ import mongoose from "mongoose";
 import { addUserAdmin } from "../utils/addAdmin";
 const connectDB = async (): Promise<void> => {
   try {
-    const mongoDBURL =
-      `${process.env.DOCKER_DATABASE_PATH}/${process.env.DATABASE_NAME}` ||
-      `mongodb://mongodb-container:27017/${process.env.DATABASE_NAME}`;
-    const connection = await mongoose.connect(
-      // `mongodb://mongodb-container:27017/${process.env.DATABASE_NAME}` // for docker
-      // `${process.env.LOCAL_PATH}/${process.env.DATABASE_NAME}` // for local
-      mongoDBURL,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000, // Reduce wait time
-      } as any
-    );
+    const DB_URL =
+      process.env.NODE_ENV === "development"
+        ? `${process.env.LOCAL_PATH}/${process.env.DATABASE_NAME}`
+        : `${process.env.LIVE_PATH}/${process.env.DATABASE_NAME}`;
+    //  `mongodb://mongodb-container:27017/${process.env.DATABASE_NAME}` //for docker
+    const connection = await mongoose.connect(DB_URL);
     addUserAdmin();
     console.log(`Connected to database : ${connection.connection.host}`);
   } catch (err: any) {
